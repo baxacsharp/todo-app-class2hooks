defb4520851d8bc6aff10210ead1b120993beacf
baxacsharp <78537820+baxacsharp@users.noreply.github.com>
2021-06-28 16:05:10 +0200

    used react hooks

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { Form } from "react-bootstrap";
 import uniqid from "uniqid";
 import { connect } from "react-redux";
@@ -8,42 +8,62 @@ const mapDispatchToProps = (dispatch) => ({
   addTodo: (todo) => dispatch(addTodo(todo))
 });
 
-class InputForm extends Component {
-  state = {
-    description: ""
+const InputForm = ({ addTodo }) => {
+
+  // [description is the value, setDescription is the setter fx] 
+  const [description, setDescription] = useState('')
+  const [user, setUser] = useState()
+
+  const handleChange = (event) => {
+    setDescription(event.target.value);
+    //description = event.target.value
   };
 
-  handleChange = (event) => {
-    this.setState({ description: event.target.value });
-  };
+  useEffect(() => {
+    console.log("updated description or user!")
+    console.log(description)
+    console.log(user)
+  }, [description, user])
 
-  handleSubmit = (event) => {
-    event.preventDefault();
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://randomuser.me/api')
+      const data = await response.json()
+
+      setUser(data.results[0])
+    }
+
+    fetchData()
+
+    return () => { console.log('unmounting component') }
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
     const todo = {
-      description: this.state.description,
+      description,
       id: uniqid(),
       completed: false
     };
 
     console.log(todo);
-    this.props.addTodo(todo);
-    this.setState({ description: "" });
+    addTodo(todo);
+    setDescription('');
   };
 
-  render() {
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <Form.Control
-          type="text"
-          placeholder="New task..."
-          value={this.state.description}
-          onChange={this.handleChange}
-        />
-        <Form.Control type="submit" />
-      </Form>
-    );
-  }
+
+  return (
+    <Form onSubmit={(e) => handleSubmit(e)}>
+      <Form.Control
+        type="text"
+        placeholder="New task..."
+        value={description}
+        onChange={(event) => handleChange(event)}
+      />
+      <Form.Control type="submit" />
+    </Form>
+  );
 }
 
 export default connect((s) => s, mapDispatchToProps)(InputForm);
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { toggleCompleted, reset } from "../store/actions";
 import { Button } from "react-bootstrap";
 import { connect } from "react-redux";
@@ -8,25 +9,25 @@ const mapDispatchToProps = (dispatch) => ({
   reset: () => dispatch(reset())
 });
 
-class List extends Component {
-  render() {
-    return (
-      <>
-        <ul>
-          {this.props.list.map((todo) => (
-            <li
-              key={todo.id}
-              onClick={() => this.props.toggleCompleted(todo)}
-              className={todo.completed ? "strikethrough" : ""}
-            >
-              {todo.description}
-            </li>
-          ))}
-        </ul>
-        <Button onClick={this.props.reset}>reset</Button>
-      </>
-    );
-  }
+const List = ({ toggleCompleted, reset, list }) => {
+
+  return (
+    <>
+      <ul>
+        {list.map((todo) => (
+          <li
+            key={todo.id}
+            onClick={() => toggleCompleted(todo)}
+            className={todo.completed ? "strikethrough" : ""}
+          >
+            {todo.description}
+          </li>
+        ))}
+      </ul>
+      <Button onClick={reset}>reset</Button>
+    </>
+  );
 }
+
 
 export default connect((s) => s, mapDispatchToProps)(List);
